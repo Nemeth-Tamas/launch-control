c825886504e1d4367c76150c2a791b07b0576675
Németh Tamás <tamas.nemeth2002@gmail.com>
2020-04-13 13:38:39 +0200

    First

---
+++ precache-manifest.5340c93c284bd5c99306b1b591f03f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac1a8e10d3110a712d34836a9989153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ed0181c9bf5063dea8",
+    "url": "/static/js/2.3636ea0a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3636ea0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2625bf86708090c5888c",
+    "url": "/static/js/main.980f7487.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
